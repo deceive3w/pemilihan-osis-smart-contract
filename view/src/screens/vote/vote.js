fa880f89e6a9a70cf49ea50e7d194d7e292c365c
rekeningku <defrykun1@gmail.com>
2019-02-25 01:25:32 +0700

    add base vote screen

--- view/src/screens/vote/vote.js
+++ view/src/screens/vote/vote.js
@@ -1,10 +1,46 @@
 import React,{Component} from 'react'
+import './styles.css'
 
+const Header = () =>(
+    <div className="header">
+        <div className="row header-container">
+            <div className="col">
+                <h2 className="header-title">Election Example using Ethereum</h2>
+            </div>
+        </div>
+    </div>
+)
+
+const Candidates = () => {
+    return(
+        <div className="candidates">
+            <div className="row candidates-row">
+                <div className="candidate border">
+                    <img src="http://scorecard.enga.ge/assets/images/candidates/clinton.jpg" className="candidate-img"/>
+                    <h6>Candidate name</h6>
+                </div>
+                <div className="candidate border ">
+                    <img src="http://scorecard.enga.ge/assets/images/candidates/clinton.jpg" className="candidate-img"/>
+                    <h6>Candidate name</h6>
+                </div>
+                <div className="candidate border">
+                    <img src="http://scorecard.enga.ge/assets/images/candidates/clinton.jpg" className="candidate-img"/>
+                    <h6>Candidate name</h6>
+                </div>
+                <div className="candidate border">
+                    <img src="http://scorecard.enga.ge/assets/images/candidates/clinton.jpg" className="candidate-img"/>
+                    <h6>Candidate name</h6>
+                </div>
+            </div>
+        </div>
+    )
+}
 class Vote extends Component {
     render(){
         return(
-            <div>
-                <h2>Vote Page</h2>
+            <div className="container">
+                <Header/>
+                <Candidates/>
             </div>
         )
     }
