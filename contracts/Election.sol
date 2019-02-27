714a5521e979b832081a708e011f73eb946ec6dc
rekeningku <defrykun1@gmail.com>
2019-02-27 23:41:52 +0700

    add register and vote logic, update test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,17 +11,44 @@ contract Election {
         //TODO: ADD AVATAR IMAGE USING IPFS
     }
 
+    struct Voter{
+        uint uid;
+        string name;
+        uint balance;
+    }
+
     uint public candidatesCount;
+    uint public votersCount;
 
     mapping(uint => Candidate) public candidates;
+    mapping(address => Voter) public voters;
 
     constructor () public {
-        //TODO: READ EXTERNAL DATA TO INITIAL DATA
         addCandidate("Calon 1");
         addCandidate("Calon 2");
+        addCandidate("Calon 3");
+        addCandidate("Calon 4");
+        addCandidate("Calon 5");
     }
     function addCandidate (string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function registerVoter(string memory _name) public returns (address){
+        votersCount++;
+        if(voters[msg.sender].uid != 0){
+            revert();
+        }
+        voters[msg.sender] = Voter(votersCount, _name, 1);
+        return msg.sender;
+    }
+
+    function vote(address from, uint candidateId) public {
+        require(voters[from].uid != 0, "Anda belum registrasi.");
+        require(voters[from].balance > 0, "Transaksi Anda gagal. Anda sudah pernah melakukan voting.");
+        require(candidates[candidateId].uid != 0, "Candidate tidak valid.");
+        candidates[candidateId].voteCount++;
+        voters[from].balance = 0;
     }
 }
--- test/election.js
+++ test/election.js
@@ -6,7 +6,7 @@ contract('Election', function(accounts){
     it('initialize candidate with to candidates', function(){
         return Election.deployed().then(async (i)=>{
             totalCandidate = await i.candidatesCount()
-            assert.equal(totalCandidate, 2)
+            assert.equal(totalCandidate, 5)
         })
     })
 
@@ -22,6 +22,42 @@ contract('Election', function(accounts){
             assert.equal(candidate2.uid, 2)
             assert.equal(candidate2.name, "Calon 2")
             assert.equal(candidate2.voteCount, 0)
+        })
+    })
+
+    it('it should can register', ()=>{
+        return Election.deployed().then(async (i)=>{
+            let voterAddress = await i.registerVoter("defri")
+            let myAddress = accounts[0]
+            let voter = await i.voters(myAddress);
+            assert.equal(voter.uid, 1);
+            assert.equal(voter.name, "defri");
+            assert.equal(voter.balance, 1);
+        })
+    })
+
+    it('it should success vote', ()=>{
+        return Election.deployed().then(async (i)=>{
+            let myAddress = accounts[0]
+            try{
+                let voterAddress = await i.vote(myAddress, 1)
+            }catch(e){
+                console.log(e)
+            }
+            let candidate = await i.candidates(1)
+            assert.equal(candidate.voteCount, 1);
+        })
+    })
+
+    it('it should cannot vote twice', ()=>{
+        return Election.deployed().then(async (i)=>{
+            let myAddress = accounts[0]
+            
+            return i.vote(myAddress, 1)
+        })
+        .then(assert.fail)
+        .catch((err)=>{
+            assert.include(err.message, 'Transaksi Anda gagal', 'Anda sudah pernah melakukan voting')
         })
     })
 })
